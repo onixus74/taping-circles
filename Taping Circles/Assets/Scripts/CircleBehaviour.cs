5527c57c306d26a62977841451c4e98c95d7d52b
onixus74 <marwen.ounis@esprit.tn>
2015-04-28 19:50:54 +0100

    Add Game Manager Add Game Manager script Add circle behaviour script

---
+++ Taping Circles/Assets/Scripts/CircleBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleBehaviour : MonoBehaviour {
+
+
+    float rScreen = 40f;
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            SpawnRandom();
+        }    	
+	}
+
+
+
+    public void SpawnRandom()
+    {
+       
+
+        Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(rScreen, Screen.width-rScreen-100), Random.Range(rScreen, Screen.height-rScreen), Camera.main.farClipPlane / 2));
+       
+
+        this.gameObject.transform.position = screenPosition;
+
+    }
+}
