2502aed27271b999e3f289d361f4a70c40333231
unleashz <mohamed.khadhr@esprit.tn>
2015-04-29 23:44:35 +0200

    Adding PauseButton (final)

--- Taping Circles/Assets/Scripts/GameManager.cs
+++ Taping Circles/Assets/Scripts/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-
+	public bool paused = false;
     // Use this for initialization
     void Start()
     {
---
+++ Taping Circles/Assets/Scripts/Paused_Button.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+
+public class Paused_Button : MonoBehaviour {
+	public bool paused = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void pause () {
+
+		paused = !paused ;
+		Time.timeScale = paused ? 0 : 1 ;
+
+		/*Time.timeScale = 0;
+		paused = true;*/
+	}
+}
