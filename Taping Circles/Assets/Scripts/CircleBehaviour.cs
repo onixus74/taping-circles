7a90252304b312ad22dcb918f3ea3d8f15b45502
onixus74 <marwen.ounis@esprit.tn>
2015-04-30 22:06:39 +0100

    Change number on wipe

--- Taping Circles/Assets/Scripts/CircleBehaviour.cs
+++ Taping Circles/Assets/Scripts/CircleBehaviour.cs
@@ -1,51 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class CircleBehaviour : MonoBehaviour {
+public class CircleBehaviour : MonoBehaviour
+{
 
 
     float rScreen = 40f;
     public Animator anim;
-   
-    
-	// Use this for initialization
-	void Start () {
+    GameObject gameManager;
+    public int ballNBR;
+
+    // Use this for initialization
+    void Start()
+    {
         anim = this.GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        gameManager = GameObject.FindGameObjectWithTag("game manager");
+        ballNBR = gameManager.GetComponent<GameManager>().ballNumber;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetButtonDown("Jump"))
         {
-            SpawnRandom();
-           
-        }  
-  	
-        
-	}
+            gameObject.GetComponent<Text>().text = gameManager.GetComponent<GameManager>().ballNumber.ToString();
+
+
+        }
+
+
+    }
 
 
 
     public void SpawnRandom()
     {
-       
 
-        Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(rScreen, Screen.width-rScreen-100), Random.Range(rScreen, Screen.height-rScreen), Camera.main.farClipPlane / 2));
+
+        Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(rScreen, Screen.width - rScreen - 100), Random.Range(rScreen, Screen.height - rScreen), Camera.main.farClipPlane / 2));
 
         anim.SetTrigger("spawn");
         this.gameObject.transform.position = screenPosition;
+        this.gameObject.GetComponentInChildren<Text>().text = gameManager.GetComponent<GameManager>().ballNumber.ToString();
 
-        
 
     }
 
     public void OnMouseDown()
     {
         anim.SetTrigger("wipe");
-      //  SpawnRandom();
+
+        gameManager.GetComponent<GameManager>().ballNumber++;
+      
+        
     }
 
-    
+
 
 
 }
