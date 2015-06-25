eefb38b753ff8d4e442c49e0de1c831c82035c1e
onixus74 <marwen.ounis@esprit.tn>
2015-06-25 15:04:12 +0100

    Change Game Behaviour
    spawn in fixed position
    spawn randomly without colliding with other

--- Taping Circles/Assets/Scripts/CircleBehaviour.cs
+++ Taping Circles/Assets/Scripts/CircleBehaviour.cs
@@ -6,56 +6,24 @@ public class CircleBehaviour : MonoBehaviour
 {
 
 
-    float rScreen = 40f;
-    public Animator anim;
-    GameObject gameManager;
-    public int ballNBR;
 
     // Use this for initialization
     void Start()
     {
-        anim = this.GetComponent<Animator>();
-        gameManager = GameObject.FindGameObjectWithTag("game manager");
-        ballNBR = gameManager.GetComponent<GameManager>().ballNumber;
+
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
-        {
-            gameObject.GetComponent<Text>().text = gameManager.GetComponent<GameManager>().ballNumber.ToString();
-
-
-        }
 
 
     }
 
 
 
-    public void SpawnRandom()
-    {
 
-
-        Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(rScreen, Screen.width - rScreen - 100), Random.Range(rScreen, Screen.height - rScreen), Camera.main.farClipPlane / 2));
-
-        anim.SetTrigger("spawn");
-        this.gameObject.transform.position = screenPosition;
-        this.gameObject.GetComponentInChildren<Text>().text = gameManager.GetComponent<GameManager>().ballNumber.ToString();
-
-
-    }
-
-    public void OnMouseDown()
-    {
-        anim.SetTrigger("wipe");
-
-        gameManager.GetComponent<GameManager>().ballNumber++;
-      
-        
-    }
 
 
 
---
+++ Taping Circles/Assets/Scripts/GameScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScript : MonoBehaviour {
+
+	//  public GameObject spawner = GameObject.Find("Spawner");
+	public GameObject circle=GameObject.Find("circle");
+	void Start () {
+		int count=0;
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 7; j++)
+			{	
+				count++;
+				GameObject tmpObj=Instantiate(Resources.Load("Spawner",typeof(GameObject)))as GameObject; 
+				tmpObj.name=count.ToString();
+				tmpObj.transform.position = new Vector3(j*1.5f-5.25f,i*-1.5f+3,9);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
