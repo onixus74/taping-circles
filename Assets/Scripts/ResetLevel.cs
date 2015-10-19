249091529de8fbad7449d943c48a1a66c5ca0500
onixus74 <marwen.ounis@esprit.tn>
2015-10-19 14:50:54 +0100

    [ADD] Show Item balance in game TODO: Show Items in menu shop

---
+++ Assets/Scripts/ResetLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Soomla.Store.IAP
+{
+public class ResetLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 this.GetComponent<Text>().text = "x"+StoreInventory.GetItemBalance("reset_level").ToString();
+	}
+}
+}
