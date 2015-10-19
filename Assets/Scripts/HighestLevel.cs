0a7fd730c9536047fc0920d35915ab6007d38c37
onixus74 <marwen.ounis@esprit.tn>
2015-10-19 10:26:16 +0100

    [ADD] Shop Panel to Menu

---
+++ Assets/Scripts/HighestLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Soomla.Store.IAP
+{
+public class HighestLevel : MonoBehaviour {
+	
+	GameManager gameManager;
+	// Use this for initialization
+	void Start () {
+	gameManager = GameObject.FindGameObjectWithTag("game manager").GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.GetComponent<Text>().text = ""+gameManager.levelShow.ToString();
+	}
+}
+}
